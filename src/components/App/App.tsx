1c4366577419de8dcfb97c395022d307c4cc2996
Luis <luisperi18@hotmail.com>
2023-05-29 20:27:06 +0200

    Add nav test

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../header/header";
 import { List } from "../list/list";
+import { NavBar } from "../nav/nav.bar";
 import styles from "./App.module.scss";
 
 export function App() {
   return (
     <div className={styles.app}>
       <Header></Header>
+      <NavBar></NavBar>
       <List></List>
     </div>
   );
---
+++ src/components/nav/nav.bar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+export function NavBar() {
+  return (
+    <>
+      <nav className="nav-bar">
+        <ul>
+          <li className="create-button"></li>
+          <button>Create</button>
+          <li className="favorite-button">
+            <button>Favorite</button>
+          </li>
+          <li className="filter-button">
+            <select>
+              <option value="film">Film</option>
+              <option value="homeworld">Homeworld</option>
+            </select>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
