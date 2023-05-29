c3e3500e59b3e5aeb208f84b359b91f8bd219e67
Marco Fratini <marcofratini@outlook.com>
2023-05-29 17:24:01 +0200

    Add restructure characterApi and api model structure

---
+++ src/services/character.repository.ts
@@ -0,0 +1,39 @@
+import { Character } from "../models/character";
+
+export class ApiRepository {
+  constructor(public url: string) {
+    this.url = "https://swapi.dev/api/people/";
+  }
+
+  async getAll() {
+    const response = await fetch(this.url);
+    if (!response.ok) {
+      const message = `Error: ${response.status}. ${response.statusText}`;
+      throw new Error(message);
+    }
+
+    return response.json();
+  }
+
+  async getCharacter(url: Character["url"]) {
+    const response = await fetch(url);
+    const search = await response.json();
+    const character: Character = {
+      id: search.url.split("/")[5],
+      name: search.name,
+      height: search.height,
+      image:
+        "https://starwars-visualguide.com/assets/img/characters/" +
+        search.url +
+        ".jpg",
+      mass: search.mass,
+      hair_color: search.hair_color,
+      eye_color: search.eye_color,
+      birth_year: search.birth_year,
+      gender: search.gender,
+      homeworld: search.homeworld,
+      url: search.url,
+    };
+    return character;
+  }
+}
