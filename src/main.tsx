34af1bcf8b29c67bf7c531295c3c355947557c13
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-05-30 10:38:14 +0200

    Apply context to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./components/App/App";
-
+import { AppContextProvider } from "./context/app.context.provider";
 
 ReactDOM.createRoot(document.querySelector(".app") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>
 );
