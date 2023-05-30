c0db4ca2587319b21de394f738d77e45f136003d
Luis <luisperi18@hotmail.com>
2023-05-30 19:58:48 +0200

    Add stylesheet module to nav and refactor nav file

--- src/components/nav.bar/nav.bar.tsx
+++ src/components/nav.bar/nav.bar.tsx
@@ -1,21 +1,30 @@
+import styles from "./nav.bar.module.scss";
 export default function NavBar() {
   return (
     <>
-      <nav className="nav-bar">
-        <ul>
-          <li>
-            <button className="create-button">Create</button>
-          </li>
-          <li>
-            <button className="favorite-button">Favorite</button>
-          </li>
-          <li>
-            <select className="filter-button">
-              <option value="film">Film</option>
-              <option value="homeworld">Homeworld</option>
-            </select>
-          </li>
-        </ul>
+      <nav className={styles.navBar}>
+        <div className="create-button">
+          <img
+            src="buttons.images/create_corasonsito.png"
+            alt="lovely stromtrooper"
+          />
+          <button>Create</button>
+        </div>
+        <div className="favorite--button">
+          <img
+            src="buttons.images/R2D2_corasonsito.png"
+            alt="lovely Arturito"
+          />
+
+          <button>Favorite</button>
+        </div>
+        <div className="navBar-rectangle">
+          <select className="filter-button">
+            <option value="">Filter</option>
+            <option value="film">Film</option>
+            <option value="homeworld">Homeworld</option>
+          </select>
+        </div>
       </nav>
     </>
   );
