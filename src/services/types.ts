8206633e15563cf661cb8a846557d380c5614e04
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-05-31 19:01:31 +0200

    Replace null type for undefined

--- src/services/types.ts
+++ src/services/types.ts
@@ -2,6 +2,6 @@ import { Character } from "../models/character";
 
 export type ApiResponse = {
   results: Character[];
-  next: string | null;
-  previous: string | null;
+  next: string | undefined;
+  previous: string | undefined;
 };
