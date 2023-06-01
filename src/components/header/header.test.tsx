0926ac388f476f7f120bf64346644ff9664c5844
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-06-01 16:04:14 +0200

    Fix testing condition for header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,14 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "./header";
+import { ContextStructure, AppContext } from "../../context/app.context";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given header component", () => {
   describe("When it is instantiate", () => {
+    const value: ContextStructure = {
+      characterContext: {
+        currentCharacter: {
+          handleLoad: jest.fn(),
+        },
+      },
+    } as unknown as ContextStructure;
+
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <AppContext.Provider value={value}>
+            <Header />
+          </AppContext.Provider>
+        </MemoryRouter>
+      );
+    });
+
     test("Then it should be in the document", () => {
-      render(<Header></Header>);
-
       const element = screen.getByRole("banner");
-
       expect(element).toBeInTheDocument();
     });
   });
