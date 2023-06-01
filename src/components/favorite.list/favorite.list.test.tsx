f121a40549fb9d02da62dd00549f678dd5d5d5c1
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-06-01 14:03:18 +0200

    Add favorite list testing condition

---
+++ src/components/favorite.list/favorite.list.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import List from "./favorite.list";
+import { Character } from "../../models/character";
+import { AppContext, ContextStructure } from "../../context/app.context";
+import { MemoryRouter } from "react-router-dom";
+
+describe("Given List component", () => {
+  const favoriteCharacters = [
+    { name: "Alex", url: "https://swapi.dev/api/people/1/" },
+  ] as Character[];
+
+  const value: ContextStructure = {
+    characterContext: {
+      favoriteCharacters,
+    },
+  } as unknown as ContextStructure;
+
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <AppContext.Provider value={value}>
+          <List></List>
+        </AppContext.Provider>
+      </MemoryRouter>
+    );
+  });
+
+  describe("When it is instantiate", () => {
+    test("Then it should be in the document", () => {
+      const element = screen.getByRole("list");
+
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
