e8cf21abfd06488f02de3d9cf2d825a3aba993e6
AlexRagno <alexragno4@gmail.com>
2023-06-01 16:58:59 +0200

    test navbar

--- src/components/nav.bar/nav.bar.test.tsx
+++ src/components/nav.bar/nav.bar.test.tsx
@@ -1,11 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import NavBar from "./nav.bar";
+import { ContextStructure, AppContext } from "../../context/app.context";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Given a navegation bar component", () => {
   describe("When it is istantiate", () => {
+    const value: ContextStructure = {
+      characterContext: {
+        currentCharacter: {
+          handleLoadLocalServer: jest.fn(),
+        },
+      },
+    } as unknown as ContextStructure;
+
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <AppContext.Provider value={value}>
+            <NavBar />
+          </AppContext.Provider>
+        </MemoryRouter>
+      );
+    });
     test("Then it should be in the document", () => {
-      render(<NavBar></NavBar>);
       const element = screen.getByRole("navigation");
       expect(element).toBeInTheDocument();
     });
