063c93cc2827644ca141c4377507b8514966f95d
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-06-02 16:57:44 +0200

    Add testing condition to cover all lines of header

--- src/components/nav.bar/nav.bar.test.tsx
+++ src/components/nav.bar/nav.bar.test.tsx
@@ -3,14 +3,15 @@ import "@testing-library/jest-dom";
 import NavBar from "./nav.bar";
 import { ContextStructure, AppContext } from "../../context/app.context";
 import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("Given a navegation bar component", () => {
   describe("When it is istantiate", () => {
     const value: ContextStructure = {
       characterContext: {
-        currentCharacter: {
-          handleLoadLocalServer: jest.fn(),
-        },
+        currentCharacter: {},
+        handleLoadLocalFavoritesServer: jest.fn(),
+        handleLoadLocalCreatedServer: jest.fn(),
       },
     } as unknown as ContextStructure;
 
@@ -23,9 +24,23 @@ describe("Given a navegation bar component", () => {
         </MemoryRouter>
       );
     });
+
     test("Then it should be in the document", () => {
       const element = screen.getByRole("navigation");
       expect(element).toBeInTheDocument();
+    });
+
+    test("Then the user should hit the returnHome button", async () => {
+      const elementCheck = screen.getAllByRole("button");
+      await userEvent.click(elementCheck[0]);
+      await userEvent.click(elementCheck[1]);
+      expect(
+        value.characterContext.handleLoadLocalFavoritesServer
+      ).toHaveBeenCalled();
+      await userEvent.click(elementCheck[2]);
+      expect(
+        value.characterContext.handleLoadLocalCreatedServer
+      ).toHaveBeenCalled();
     });
   });
 });
