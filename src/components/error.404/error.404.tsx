93460a207dcf1c6dffa4b747e2fde9becc69a3eb
Luis <luisperi18@hotmail.com>
2023-06-02 22:10:21 +0200

    Add 404 desktop

--- src/components/error.404/error.404.tsx
+++ src/components/error.404/error.404.tsx
@@ -5,19 +5,31 @@ export default function Error404() {
     <>
       <div className={styles.errorContainer}>
         <img
+          className={styles.deathStar}
+          src="/background.images/star-wars-the-clone-wars-death-star-wookieepedia-death-star-d37d722158b5324e83398f2249337dc5.png"
+          alt="the death star"
+          width={803}
+          height={803}
+        />
+
+        <img
           className={styles.droidsImage}
           src="/background.images/pngeggdasdas.png"
           alt="arturito and gold gay robot"
           width={326}
           height={377}
         />
+        <p className={styles.hidingParagraphDeathStar}>
+          404 Error... the page you’re looking for doesn’t exist
+        </p>
+        <div className={styles.hidingParagraphGayRobots}>
+          <p>These aren't the</p>
+          <p>Doids you're</p>
+          <p>looking for...</p>
 
-        <p>These aren't the</p>
-        <p>Doids you're</p>
-        <p>looking for...</p>
-
-        <span>404Error</span>
-        <span>page not found</span>
+          <span>404Error</span>
+          <span>page not found</span>
+        </div>
       </div>
     </>
   );
