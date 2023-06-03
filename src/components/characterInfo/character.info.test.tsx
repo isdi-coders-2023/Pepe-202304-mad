cb54c98efcac04f124045e05b1095bcae91180e5
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-06-03 13:57:52 +0200

    Cover line 34 with second testing condition of character info test

--- src/components/characterInfo/character.info.test.tsx
+++ src/components/characterInfo/character.info.test.tsx
@@ -5,7 +5,7 @@ import { ContextStructure, AppContext } from "../../context/app.context";
 import { MemoryRouter } from "react-router-dom";
 
 describe("Given the component character info", () => {
-  describe("When it is instantiated", () => {
+  describe("When feedbackMessage is provided", () => {
     const mockCharacter = {
       name: "alex",
       height: "44",
@@ -21,6 +21,7 @@ describe("Given the component character info", () => {
     const value: ContextStructure = {
       characterContext: {
         currentCharacter: mockCharacter,
+        feedbackMessage: "Character added successfully",
       },
     } as unknown as ContextStructure;
 
@@ -34,9 +35,49 @@ describe("Given the component character info", () => {
       );
     });
 
-    test("Then it should should be in the document", () => {
-      const element = screen.getByRole("img");
-      expect(element).toBeInTheDocument();
+    test("Then the feedback container should have visible class", () => {
+      const element = screen.getByText(
+        "CHARACTER ADDED TO FAVORITES ❤️"
+      ).parentElement;
+      expect(element).toHaveClass("visible");
+    });
+  });
+
+  describe("When feedbackMessage is not provided", () => {
+    const mockCharacter = {
+      name: "alex",
+      height: "44",
+      image: "url",
+      mass: "44",
+      hair_color: "blue",
+      eye_color: "blue",
+      birth_year: "1999",
+      gender: "other",
+      homeworld: "Catalunya",
+    };
+
+    const value: ContextStructure = {
+      characterContext: {
+        currentCharacter: mockCharacter,
+        feedbackMessage: null,
+      },
+    } as unknown as ContextStructure;
+
+    beforeEach(() => {
+      render(
+        <MemoryRouter>
+          <AppContext.Provider value={value}>
+            <CharacterInfo />
+          </AppContext.Provider>
+        </MemoryRouter>
+      );
+    });
+
+    test("Then the feedback container should not have visible class", () => {
+      const element = screen.getByText(
+        "CHARACTER ADDED TO FAVORITES ❤️"
+      ).parentElement;
+      expect(element).not.toHaveClass("visible");
     });
   });
 });
